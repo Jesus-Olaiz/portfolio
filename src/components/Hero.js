3d4b11f864882c8f7fa76b433d528574dccf0f59
mended-marlin <felix71679@gmail.com>
2023-10-15 22:24:49 -0400

    just basic set up

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+
+
+const Hero = (props) => {
+
+    return (
+        <div className='hero'>This is a hero</div>
+    )
+
+
+} 
+
+
+
+
+export default Hero
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+
+export default function Nav (props) {
+    
+    return (
+        <nav>
+            <h1>Jesus Olaiz</h1>
+
+            <div className="links">
+                <a className="link" href='/'>link</a>
+            </div>
+        </nav>
+    )
+
+}
